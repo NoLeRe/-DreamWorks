302ae9097f6eb23d07b6e74afae562420e70cb88
NoLeRe <1179451475@qq.com>
2019-11-18 15:01:51 +0800

    添加文件操作

---
+++ Contanct_3.0/Contanct/contact.c
@@ -0,0 +1,169 @@
+#include"Contact.h"
+
+void init_contact(pcontact pc)
+{
+	pc->size = 0;
+}
+
+void add_contact(pcontact pc)
+{
+	info cur;
+	if (pc->size == data_MAX)
+	{
+		printf("人员已满！添加失败！\n");
+		return;
+	}
+	printf("请输入姓名：\n");
+	scanf("%s", cur.name);
+	printf("请输入性别：\n");
+	scanf("%s", cur.gender);
+	printf("请输入年龄：\n");
+	scanf("%d", &cur.age);
+	printf("请输入号码：\n");
+	scanf("%s", cur.tele);
+	printf("请输入地址：\n");
+	scanf("%s", cur.addr);
+	pc->data[pc->size] = cur;
+	pc->size++;
+	
+}
+void del_contact(pcontact pc)
+{
+	char name[name_MAX];
+	printf("请输入要删除的姓名：\n");
+	scanf("%s", name);
+	int pos = find_contact(pc, name);
+	if (pos == -1)
+	{
+		printf("查无此人！删除失败\n");
+		return;
+	}
+	for (int i = pos; i < pc->size; i++)
+	{
+		pc->data[i] = pc->data[i + 1];
+	}
+	pc->size--;
+	printf("删除完成！\n");
+}
+
+int  find_contact(pcontact pc, char *name)
+{
+	for (int i = 0; i < pc->size; i++)
+	{
+		if (strcmp(pc->data[i].name, name) == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+
+}
+void ser_contact(pcontact pc)
+{
+	char name[name_MAX];
+	printf("请输入你需要查找的姓名：\n");
+	scanf("%s", name);
+	int pos = find_contact(pc, name);
+	if (pos == -1)
+	{
+		printf("查无此人！\n");
+		return;
+	}
+	printf("=========================\n");
+	printf("姓名：%s\n", pc->data[pos].name);
+	printf("性别：%s\n", pc->data[pos].gender);
+	printf("年龄：%d\n", pc->data[pos].age);
+	printf("电话号码：%s\n", pc->data[pos].tele);
+	printf("地址：%s\n", pc->data[pos].addr);
+	printf("=========================\n");
+
+}
+void mod_contact(pcontact pc)
+{
+	char name[name_MAX];
+	printf("请输入你需要修改的姓名：\n");
+	scanf("%s", name);
+	int pos = find_contact(pc, name);
+	if (pos == -1)
+	{
+		printf("查无此人！修改失败！\n");
+		return;
+	}
+	printf("请输入姓名：\n");
+	scanf("%s", pc->data[pos].name);
+	printf("请输入性别：\n");
+	scanf("%s", pc->data[pos].gender);
+	printf("请输入年龄：\n");
+	scanf("%d", &pc->data[pos].age);
+	printf("请输入号码：\n");
+	scanf("%s", pc->data[pos].tele);
+	printf("请输入地址：\n");
+	scanf("%s", pc->data[pos].addr);
+
+}
+void check_contact(pcontact pc)
+{
+	
+	sort_contact(pc);
+	for (int i = 0; i < pc->size; i++)
+	{
+		printf("=========================\n");
+		printf("姓名：%s\n", pc->data[i].name);
+		printf("性别：%s\n", pc->data[i].gender);
+		printf("年龄：%d\n", pc->data[i].age);
+		printf("电话号码：%s\n", pc->data[i].tele);
+		printf("地址：%s\n", pc->data[i].addr);
+		printf("=========================\n");
+		printf("\n");
+	}
+}
+void sort_contact(pcontact pc)
+{
+	for (int sz = pc->size; sz > 0; sz--)
+	{
+		for (int i = 0; i < sz - 1; i++)
+		{
+			if (strcmp(pc->data[i].name, pc->data[i + 1].name)>0)
+			{
+				info temp   = pc->data[i];
+				pc->data[i] = pc->data[i + 1];
+				pc->data[i + 1] = temp;
+			}
+		}
+	}
+
+}
+void load_contact(pcontact pc)
+{
+	FILE* pf = fopen("contact.dat", "rb");
+	if (pf == NULL)
+	{
+		printf("打开文件失败！\n");
+		return;
+	}
+	info curinfo;
+	while (fread(&curinfo, sizeof(info), 1, pf))
+	{
+		if (pc->size)
+		{
+			pc->data[pc->size] = curinfo;
+			pc->size++;
+		}
+	}
+	fclose(pf);
+}
+void save_contact(pcontact pc)
+{
+	FILE* pf = fopen("contact.dat", "wb");
+	if (pf == NULL)
+	{
+		printf("打开文件失败！\n");
+		return;
+	}
+	for (int i = 0; i < pc->size; i++)
+	{
+		fwrite(pc->data + i, sizeof(info), 1, pf);
+
+	}
+	fclose(pf);
+}
---
+++ Contanct_3.0/Contanct/test.c
@@ -0,0 +1,60 @@
+#include"Contact.h"
+
+void menu()
+{
+	printf("============================\n");
+	printf("==0.退出======1.添加========\n");
+	printf("==2.删除======3.查找========\n");
+	printf("==4.更改======5.清空========\n");
+	printf("==6.查看======7.加载========\n");
+	printf("==8.存储====================\n");
+}
+
+int main()
+{
+	contact ct;
+	int n;
+	init_contact(&ct);
+	do{
+		menu();
+		printf("请选择选项：-》 ");
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 0:
+			break;
+		case 1:
+			add_contact(&ct);
+			break;
+		case 2:
+			del_contact(&ct);
+			break;
+		case 3:
+			ser_contact(&ct);
+			break;
+		case 4:
+			mod_contact(&ct);
+			break;
+		case 5:
+			init_contact(&ct);
+			break;
+		case 6:
+			check_contact(&ct);
+			break;
+		case 7:
+			load_contact(&ct);
+			break;
+		case 8:
+			save_contact(&ct);
+			break;
+		default:
+			printf("无效选项，请重新输入：\n");
+			break;
+		}
+
+	} while (n);
+
+
+	system("pause");
+	return 0;
+}
